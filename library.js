c8ae7aefc50904cd46d59eab927f7c8ffa569d5e
Adam Cevallos <adamcevallos238@gmail.com>
2021-07-08 22:21:22 -0500

    Crated button to add books along with with a form, no functionality

--- library.js
+++ library.js
@@ -1,20 +1,29 @@
-let myLibrary = [];
+// let myLibrary = [];
 
-function Book(title, author, numPages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;   
-    this.isRead = isRead;
+function openAddForm() {
+    document.getElementById("add-form").style.display = "block";
+    document.querySelectorAll("body > *:not(#add-form)").forEach
+        (element => element.classList.toggle('blur'));
+}
+function closeAddForm() {
+    document.getElementById("add-form").style.display = "none";
+    document.querySelectorAll("body > *:not(#add-form)").forEach
+        (element => element.classList.toggle('blur'));
 }
 
-Book.prototype.info = function() {
-    let readString = (this.isRead) ? 'Has been read.' : 'Has not been read yet.';
-    return `${this.title} by ${this.author}, ${this.numPages} pages. ${readString}`;
-}
-
-// function addBookToLibrary() {
-//     for (let i = 0; i < myLibrary.length; i++) {
-
-//     }
+// function Book(title, author, numPages, isRead) {
+//     this.title = title;
+//     this.author = author;
+//     this.numPages = numPages;   
+//     this.isRead = isRead;
 // }
 
+// Book.prototype.info = function() {
+//     let readString = (this.isRead) ? 'Has been read.' : 'Has not been read yet.';
+//     return `${this.title} by ${this.author}, ${this.numPages} pages. ${readString}`;
+// }
+
+// const got = new Book('Game of Thrones mothertrucker', 'George R.R Martin', 694, false);
+// myLibrary.push(got);
+
+// console.log(myLibrary);
